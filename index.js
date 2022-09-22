9f79780a72a76612182f39d9afbf38be1f217ba5
Cesar Julian <trabajos.cesar23@gmail.com>
2022-09-22 14:08:07 -0500

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const product = require("./api/products");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/product", product);
+
+app.listen(PORT, () => console.log(`Server is runnig in port ${PORT}`));
